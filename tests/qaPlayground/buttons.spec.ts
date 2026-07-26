8137a8b4d916ef061aea8b7484cfb0785a7862d7
klindukhov <61867600+klindukhov@users.noreply.github.com>
2026-07-26 23:05:21 +0200

    feat: add button tests

---
+++ tests/qaPlayground/buttons.spec.ts
@@ -0,0 +1,140 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://qaplayground.com/practice/buttons");
+});
+
+test.describe("Home button", () => {
+  test("should navigate to home page", async ({ page }) => {
+    await page.locator("#navigateHomeBtn").click();
+    expect(page.locator("#result-s01")).toHaveText("Navigated to Home Page");
+  });
+});
+
+test.describe("Find location button", () => {
+  test("should cause the button location to be displayed", async ({ page }) => {
+    const button = page.locator("#coordinatesBtn");
+    await button.click();
+    const location = await button.boundingBox();
+
+    expect(page.locator("#result-s02")).toHaveText(
+      `X: ${Math.round(location?.x || 0)}px, Y: ${Math.round(location?.y || 0)}px`,
+    );
+  });
+
+  test("should update the location information after scrolling", async ({
+    page,
+  }) => {
+    const button = page.locator("#coordinatesBtn");
+    await button.click();
+    await page.mouse.wheel(0, 10);
+
+    await button.click();
+
+    const location = await button.boundingBox();
+
+    expect(page.locator("#result-s02")).toHaveText(
+      `X: ${Math.round(location?.x || 0)}px, Y: ${Math.round(location?.y || 0)}px`,
+    );
+  });
+});
+
+test.describe("Color button", () => {
+  test("should cause its color to be display in th result field", async ({
+    page,
+  }) => {
+    const button = page.locator("#colorBtn");
+
+    const color = await button.evaluate(
+      (element) => getComputedStyle(element).backgroundColor,
+    );
+    await button.focus();
+    await page.keyboard.press("Enter");
+
+    expect(page.locator("#result-s03")).toHaveText(`Background: ${color}`);
+  });
+});
+
+test.describe("Size button", () => {
+  test("should cause the button size to get displayed", async ({ page }) => {
+    const button = page.locator("#sizeBtn");
+    await button.click();
+    const buttonSize = await button.boundingBox();
+    expect(page.locator("#result-s04")).toHaveText(
+      `W: ${buttonSize?.width}px, H: ${buttonSize?.height}px`,
+    );
+  });
+});
+
+test.describe("Disabled button", () => {
+  test("should be disabled", async ({ page }) => {
+    expect(page.locator("#disabledBtn")).toBeDisabled();
+  });
+});
+
+test.describe("Click and hold button", () => {
+  test("should cause the success text to be displayed on click and hold", async ({
+    page,
+  }) => {
+    await page.locator("#clickHoldBtn").hover();
+    await page.mouse.down();
+    setTimeout(async () => {
+      await page.mouse.up();
+      expect(page.locator("#result-s06")).toHaveText("Held for 1.5s");
+    }, 1500);
+  });
+
+  test("should cause the hold text to be displayed while holding", async ({
+    page,
+  }) => {
+    await page.locator("#clickHoldBtn").hover();
+    await page.mouse.down();
+    expect(page.locator("#result-s06")).toHaveText("Holding... keep pressing");
+    setTimeout(async () => {
+      await page.mouse.up();
+    }, 1500);
+  });
+
+  test("should cause the try again text to be displayed on single click", async ({
+    page,
+  }) => {
+    await page.locator("#clickHoldBtn").click();
+    expect(page.locator("#result-s06")).toHaveText(
+      "Released too early - hold for 1.5s",
+    );
+  });
+});
+
+test.describe("Double Click button", () => {
+  test("should cause the value to be displayed on double click", async ({
+    page,
+  }) => {
+    await page.locator("#doubleClickBtn").dblclick();
+
+    expect(page.locator("#result-s07")).toHaveText("Double clicked!");
+  });
+
+  test("should not change the value on single click", async ({ page }) => {
+    await page.locator("#doubleClickBtn").click();
+
+    expect(page.locator("#result-s07")).toHaveText("Not double-clicked yet");
+  });
+});
+
+test.describe("Right click button", () => {
+  test("should cause the text to change on right click", async ({ page }) => {
+    await page.locator("#rightClickBtn").hover();
+    await page.mouse.down({ button: "right" });
+    await page.mouse.up();
+
+    expect(page.locator("#result-s08")).toHaveText("Context menu triggered!");
+  });
+
+  test("should not cause the text to change on single click", async ({
+    page,
+  }) => {
+    await page.locator("#rightClickBtn").click();
+
+    expect(page.locator("#result-s08")).toHaveText("No action performed yet");
+  });
+});
