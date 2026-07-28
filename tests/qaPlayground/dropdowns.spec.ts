b8ab82ccf9b4a67c80f859ef55c11789dc622014
klindukhov <61867600+klindukhov@users.noreply.github.com>
2026-07-28 23:28:40 +0200

    feat: add partial dropdowns tests

---
+++ tests/qaPlayground/dropdowns.spec.ts
@@ -0,0 +1,144 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  page.goto("https://qaplayground.com/practice/dropdowns");
+});
+
+test.describe("Select Fruit by Visible Text dropdown", () => {
+  test("should get the option selected by visible text", async ({ page }) => {
+    await page.locator("#fruitSelect").selectOption({ label: "Orange" });
+    expect(page.locator("#result-s01")).toHaveText("Selected fruit: Orange");
+
+    await page.locator("#fruitSelect").selectOption({ label: "Apple" });
+    expect(page.locator("#result-s01")).toHaveText("Selected fruit: Apple");
+
+    await page.locator("#fruitSelect").selectOption({ label: "Banana" });
+    expect(page.locator("#result-s01")).toHaveText("Selected fruit: Banana");
+  });
+
+  test("should treat Select Fruit as a non-choice", async ({ page }) => {
+    await page.locator("#fruitSelect").selectOption({ label: "Select Fruit" });
+    expect(page.locator("#result-s01")).toHaveText("No fruit selected");
+  });
+});
+
+test.describe("Select Country by Value Attribute dropdown", () => {
+  test("should have 5 options in the dropdown", async ({ page }) => {
+    await page.locator("#countrySelect").click();
+    const options = await page.locator("#countrySelect option").allInnerTexts();
+    expect(options.length).toBe(5);
+  });
+
+  test("should get the option selected by value", async ({ page }) => {
+    await page.locator("#countrySelect").selectOption({ value: "argentina" });
+    expect(page.locator("#result-s02")).toHaveText(
+      "Selected country: Argentina (argentina)",
+    );
+
+    await page.locator("#countrySelect").selectOption({ value: "india" });
+    expect(page.locator("#result-s02")).toHaveText(
+      "Selected country: India (india)",
+    );
+
+    await page.locator("#countrySelect").selectOption({ value: "japan" });
+    expect(page.locator("#result-s02")).toHaveText(
+      "Selected country: Japan (japan)",
+    );
+
+    await page
+      .locator("#countrySelect")
+      .selectOption({ value: "united-states" });
+    expect(page.locator("#result-s02")).toHaveText(
+      "Selected country: United States (united-states)",
+    );
+  });
+
+  test("should treat Select Country as a non-choice", async ({ page }) => {
+    await page.locator("#countrySelect").selectOption({ value: "" });
+    expect(page.locator("#result-s02")).toHaveText("No country selected");
+  });
+});
+
+test.describe("Select Last Language and Read All Options", () => {
+  test("should have all the expected options available", async ({ page }) => {
+    await page.locator("#languageSelect").click();
+    const options = await page
+      .locator("#languageSelect option")
+      .allInnerTexts();
+
+    expect(page.locator("#result-s03")).toHaveText(
+      `Languages available: ${options.length}`,
+    );
+  });
+
+  test("should get the option displayed in result when selected", async ({
+    page,
+  }) => {
+    await page.locator("#languageSelect").click();
+    const options = await page
+      .locator("#languageSelect option")
+      .allInnerTexts();
+
+    await page
+      .locator("#languageSelect")
+      .selectOption({ label: options[options.length - 1] });
+
+    expect(page.locator("#result-s03")).toHaveText(
+      `Selected language: TypeScript`,
+    );
+  });
+
+  test("should get the appropriate message displayed when the button is pressed", async ({
+    page,
+  }) => {
+    await page.locator("#selectLastLanguageBtn").click();
+
+    expect(page.locator("#result-s03")).toHaveText(
+      `Selected TypeScript; options: Python, Java, JavaScript, TypeScript`,
+    );
+    expect(await page.locator("#languageSelect").inputValue()).toBe(
+      "typescript",
+    );
+  });
+});
+
+test.describe("Multi-select superheroes", () => {
+  test("should get selected option displayed when selected", async ({
+    page,
+  }) => {});
+  test("should get all selected options displayed when selected", async ({
+    page,
+  }) => {});
+  test("should get selected option removed when deselected", async ({
+    page,
+  }) => {});
+});
+
+test.describe("Custom Dropdown Listbox", () => {
+  test("should have default text displayed as placeholder", async ({
+    page,
+  }) => {});
+  test("should display the text and value in the dropdown", async ({
+    page,
+  }) => {});
+  test("should get the selected option displayed", async ({ page }) => {});
+  test("should not have the default option in the dropdown", async ({
+    page,
+  }) => {});
+});
+
+test.describe("Searchable City Combobox", () => {
+  test("should have placeholder visible by default", async ({ page }) => {});
+  test("should have all options available when no input is provided", async ({
+    page,
+  }) => {});
+  test("should filter only relevant options when some input is provided", async ({
+    page,
+  }) => {});
+  test("should get the option selected with keyboard", async ({ page }) => {});
+  test("should get the option selected on click", async ({ page }) => {});
+  test("should display no matches found with invalid input", async ({
+    page,
+  }) => {});
+  test("", async ({ page }) => {});
+});
